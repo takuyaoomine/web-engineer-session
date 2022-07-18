6a21b1d5909ae8431f8d6d42c8358a6816ba2ed7
takuyaOmine <bowwowtaro@gmail.com>
2022-07-18 18:05:56 +0900

    feat : add calc num^3

--- app/api.php
+++ app/api.php
@@ -7,8 +7,10 @@ if(isset($_GET["num"]) && !preg_match('/[^0-9]/', $_GET["num"])) {
     $param = htmlspecialchars($_GET["num"]);
     // メイン処理
     $arr["status"] = "OK";
-    $arr["num_x_3"] = (string)((int)$param * 3); // num x 3
-    $arr["num_+_num"] = (string)((int)$param * (int)$param); // num + num
+    $arr["num"] = (string)((int)$param * 3); // num
+    $arr["num_x_num"] = (string)((int)$param * (int)$param); // num * num
+    $arr["num_x_num_x_num"] = (string)((int)$param * (int)$param * (int)$param); // num * num
+
 } else {
     // paramの値が不適ならstatusをnoにしてプログラム終了
     $arr["status"] = "NG";
